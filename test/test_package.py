53f2323043cd2da937d93cedf1a6d52a67c17983
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-09-02 17:10:16 +0200

    Initial Commit

---
+++ test/test_package.py
@@ -0,0 +1,7 @@
+from examon_core.examon_item_registry import ExamonItemRegistry
+from examon_pcep_package.questions import *
+
+
+class TestInputParameterQuestion:
+    def test_repo(self):
+        assert len(ExamonItemRegistry.registry()) == 1
