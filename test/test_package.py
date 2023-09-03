fd1a940b51e04351b2f135b2a9aae4ace5ae8b29
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-09-03 12:07:34 +0200

    First draft

--- test/test_package.py
+++ test/test_package.py
@@ -1,7 +1,7 @@
-from examon_core.examon_item_registry import ExamonItemRegistry
+from examon_core.in_memory_db import InMemoryDB
 from examon_pcep_package.questions import *
 
 
 class TestInputParameterQuestion:
     def test_repo(self):
-        assert len(ExamonItemRegistry.registry()) == 43
+        assert len(InMemoryDB.load()) == 43
